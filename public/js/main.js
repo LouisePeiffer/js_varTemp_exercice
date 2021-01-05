8ac566f525414a1c63f10fd7f982d563b0efb272
Louise <louisepeiffer@hotmail.com>
2021-01-05 10:48:25 +0100

    fini

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+// Variables temporaire
+// let a = 1;
+// let b = 2; 
+// let c = 3; 
+// a = b
+// b = c
+// let temp;
+// temp = 1
+// c = temp
+// console.log(a, b, c);
+
+// Exo 1
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// a = d
+// b = c
+// let temp1;
+// temp1 = 2
+// let temp2;
+// temp2 = 1
+// c = temp1
+// d = temp2
+// console.log(a, b, c, d);
+
+// Exo 2
+// let a = 1; 
+// let b = 2; 
+// let c = 3; 
+// let d = 4; 
+
+// temp1 = a
+
+// a = b
+// b = c
+// c = d
+// d = temp1
+
+// console.log(a, b, c, d);
+
+// Exo 3
+// let a = 4; 
+// let b = 3; 
+// let c = 2; 
+// let d = 1; 
+
+// let tableau = [a, b, c, d]
+// console.log(tableau.reverse());
+
+// Exo 4
+let a = 1 
+let b = 2
+let c = 3
+let d = 4
+let e = 5
+let f = 6
+
+temp1 = a
+temp2 = b
+temp3 = c
+
+a = d
+b = e
+c = temp1
+d = temp2
+e = f
+f = temp3
+
+console.log(a,b,c,d,e,f);
